16e61803e63c7c7ef88da4e456042f3103382180
Loic Yvonnet <loic.yvonnet.pro@gmail.com>
2017-08-22 23:13:12 +0200

    Initial Commit

---
+++ randomize.cpp14/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  randomize.cpp14
+//
+//  Created by LOIC YVONNET on 22/08/2017.
+//  Copyright © 2017 LOIC YVONNET. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
